8e507bbd184496106d862dfaf39b29056a641cb1
taoynkkx <taoynkkx@163.com>
2020-05-08 22:50:51 +0800

    #1 config LED outputs and KEY inputs

--- Software/src/Core/Src/main.c
+++ Software/src/Core/Src/main.c
@@ -20,6 +20,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -48,7 +49,6 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -150,19 +150,6 @@ void SystemClock_Config(void)
   /** Configure the Systick interrupt time 
   */
   __HAL_RCC_PLLI2S_ENABLE();
-}
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOD_CLK_ENABLE();
-
 }
 
 /* USER CODE BEGIN 4 */
